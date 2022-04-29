839f209c40e23e8c81dfb86440931e10440d238a
pietroline <pietrobarberi@hotmail.it>
2022-04-29 17:37:04 +0200

    preparato work flow e integrazione vue front-end

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>404</h1>
+        <h3>Pagina non trovata</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::middleware("auth")
+    ->namespace("Admin")
+    ->name("admin.")
+    ->prefix("admin")
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('home');
+    });
+
+Route::get("{any?}", function(){
+    return view("guest.home");
+})->where("any", ".*");
+
+
