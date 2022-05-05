cc20fecd45b26347b20260bf284c27932c338256
pietroline <pietrobarberi@hotmail.it>
2022-05-05 10:55:42 +0200

    feat register admin

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::middleware("auth")
     ->group(function(){
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('foods', 'FoodController');
+        Route::resource('restaurants', 'RestaurantController');
     });
 
 Route::get("{any?}", function(){
