8c47fcb8bbef68cde0c82d434aa581e7270126f8
AlessandroImpact <terracciano41@gmail.com>
2022-05-12 17:10:48 +0200

    banner funziona su pc e tablet

--- resources/js/components/partials/BannerWorkForUs.vue
+++ resources/js/components/partials/BannerWorkForUs.vue
@@ -1,18 +1,19 @@
 <template>
   
-<b-container fluid class=" my-4">
-  <b-row>
-    <b-col class="text-center">
-        <h1>Hai già la nostra App?</h1>
-        <h4 class="py-3">Scaricala ora - disponibile su Apple store e Google Play!</h4>
+
+<b-container fluid class="my-3">
+  <b-row class="mx-auto justify-content-center align-items-center">
+    <b-col cols="4" class="text-center">
+        <h1>Hai già la nostra <span class="ms_orange">App?</span> </h1>
+        <h4 class="py-3">Scaricala <span class="ms_orange">ora</span>  - disponibile su Apple store e Google Play!</h4>
 
         <b-row>
             <b-col>
-                <b-img src="../../../../public/img/LOGO APP STORE" fluid alt="logo app store"></b-img>
+                <b-img :src="require('../../../../public/img/app-store.png')" fluid alt="logo app store"></b-img>
             </b-col>
 
             <b-col>
-                <b-img src="../../../../public/img/LOGO PLAY STORE" fluid alt="logo play store"></b-img>
+                <b-img :src="require('../../../../public/img/play-store.png')" fluid alt="logo play store"></b-img>
             </b-col>
         </b-row>
         
@@ -20,31 +21,45 @@
        
        
     </b-col>
-    <b-col>
-        <b-img src="../../../../public/img/banner-deliveboo.svg" fluid alt="Responsive image"></b-img>
+    <b-col cols="4" class="ms_ban ">
+        <b-img class="ms-img"  :src="require('../../../../public/img/banner-deliveboo.png')" fluid alt="Responsive image"></b-img>
     </b-col>
     
   </b-row>
 
   <!--SECONDO BANNER-->
 
-  <b-row>
-    <b-col class="text-center">
+  <b-row class="my-4 justify-content-center rounded">
+    <b-col class="ms_ban" cols="4">
 
-         <b-img src="../../../../public/img/banner-deliveboo2.svg" fluid alt="Responsive image"></b-img>
+         <b-img :src="require('../../../../public/img/banner-deliveboo-2.png')" fluid alt="Responsive image"></b-img>
 
     </b-col>
-    <b-col>
+    <b-col  cols="4" class=" d-flex ms_bg-dOrange justify-content-center align-items-center rounded text-center ">
 
-           <h1>DeliveBoo for Work</h1>
-        <h4 class="py-3">Inizia ora a guadagnare!</h4>
+      <div>
+         <h1 class="text-dark ms_bolder">DeliveBoo for <span class="text-white">Work</span> </h1>
 
-        <b-button class="ms-btn-banner" href="#">Contattaci</b-button>
+  <h4 class="my-2 text-dark">Inizia ora a guadagnare!</h4>
+     
+    
+       
+            <b-button class="ms-btn-banner" href="#">Contattaci</b-button>
+
+      </div>
+              
+      
+       
+      
+     
 
     </b-col>
     
   </b-row>
+
+
 </b-container>
+
 
 
 
@@ -68,6 +83,34 @@ name:'BannerWorkForUs',
     border: 1px solid $lightOrange;
     color: white;
     font-weight: bold;
+
+    &:hover{
+       background-color: white;
+    border: 1px solid $lightOrange;
+    color:$darkOrange;
+    }
 }
 
+
+.ms_ban{
+  border: 1px solid $darkOrange;
+  border-radius: 5px;
+}
+
+.ms_img{
+  width: 100%;
+  height: 100%;
+}
+
+.ms_orange{
+  color: $darkOrange;
+}
+
+.ms_bg-dOrange{
+  background-color: $darkOrange;
+}
+
+.ms_bolder{
+  font-weight: bolder;
+}
 </style>
