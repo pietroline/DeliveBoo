747a2ac6291248fe2eabb75d93adbc067cfb3f90
MyUsernme <gabriele.morelli@live.it>
2022-05-12 16:40:27 +0200

    prime modifiche

--- public/js/functionsJS/validatorCheckbox.js
+++ public/js/functionsJS/validatorCheckbox.js
@@ -129,7 +129,7 @@ if (checkboxes) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/pietroline/Scrivania/Personale/boolean/GitHub/progettoFinale/DeliveBoo/resources/js/functionsJS/validatorCheckbox.js */"./resources/js/functionsJS/validatorCheckbox.js");
+module.exports = __webpack_require__(/*! C:\Users\emanuela\Desktop\boolean\Progetto Finale\DeliveBoo\DeliveBoo\DeliveBoo\resources\js\functionsJS\validatorCheckbox.js */"./resources/js/functionsJS/validatorCheckbox.js");
 
 
 /***/ })
--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,48 +1,76 @@
 <template>
-    <header class="container-fluid">
-            <nav class="navbar navbar-expand navbar-light bg-light">
+<header>
 
-                <router-link class="navbar-brand" :to="{name: 'home'}">DeliveBoo</router-link>
-            
-                <ul class="navbar-nav mr-auto">
+        <b-navbar toggleable="lg">
+         <router-link class="navbar-brand" :to="{name: 'home'}"><a href="#"><img src="./../../../public/img/logo-white.png" class="img" alt=""/></a></router-link>
 
-                    <li class="nav-item active" v-for="itemMenu in navMenu" :key="itemMenu.routeName">
-                        <router-link class="nav-link" :to="{name: itemMenu.routeName}">{{itemMenu.label}}</router-link>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+             <b-collapse id="nav-collapse" is-nav>
+
+               <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item" v-for="itemMenu in navMenu" :key="itemMenu.routeName">
+                        <router-link class="ms_white" :to="{name: itemMenu.routeName}">{{itemMenu.label}}</router-link>
                     </li>
 
                 </ul>
 
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/admin">Area Admin</a>
-                    </li>
-                </ul>
+                
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-form>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="ms_white" href="/admin">Accedi</a>
+                        </li>
+                    </ul>
+                </b-nav-form>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
 
-            </nav>
-    </header>
+</header>
+
+   
+
 </template>
 
+  
+
 <script>
-    export default {
-        name: "Header",
+export default {
+  name: "Header",
 
-        data(){
-            return{
-                navMenu: [
-                    {
-                        routeName: "home",
-                        label: "Home"
-                    },
-                ],
-               
-
-            }
+  data() {
+    return {
+      navMenu: [
+        {
+          routeName: "home",
+          label: "Home",
         },
-
-    }
+      ],
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+ 
+ header{
+   background-color: #ff8c28;
+   height: 80px;
+ }
 
+ img{
+    
+ }
 
+ .ms_white{
+   color: #fff;
+   font-weight: bold;
+   font-size: 20px;
+ }
 </style>
+
+
+ 
