68c1ca286437d6d5ae6caba411123ee64b6fc6b7
AlessandroImpact <terracciano41@gmail.com>
2022-05-12 09:15:37 +0200

    iniziata stilizzazione della pagina

--- resources/js/components/partials/Restaurant.vue
+++ resources/js/components/partials/Restaurant.vue
@@ -2,13 +2,13 @@
 
     <div class="card my-3">
 
-        <!-- <img src="..." class="card-img-top" alt="..."> -->
+         <img src="https://picsum.photos/150/100" class="card-img-top" alt=""> 
         
         <div class="card-body ms_card">
             <h5 class="card-title">{{name}}</h5>
         
             <div class="d-flex justify-content-center my-4">
-                <router-link class="btn btn-primary ms_btn" :to="{name: 'single-restaurant', params: {slug: slug}}">Vai al Ristorante</router-link>
+                <router-link class="btn ms_btn" :to="{name: 'single-restaurant', params: {slug: slug}}">Vai al Ristorante</router-link>
             </div>
         </div>
     </div>
@@ -39,6 +39,8 @@
     border: 1px solid $lightOrange;
     color: white;
     font-weight: bold;
+    
+    
 }
 
 </style>
