9efe4e5efcb777116e5f742725d502bde8b0a813
MyUsernme <gabriele.morelli@live.it>
2022-05-13 10:58:39 +0200

    ultime modifiche

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,38 +1,29 @@
 <template>
-<header>
+  <header>
+  <b-navbar toggleable="lg">
+    <router-link class="navbar-brand" :to="{name: 'home'}"><a href="#"><img src="./../../../public/img/logo-white.png" class="img" alt=""/></a></router-link>
 
-        <b-navbar toggleable="lg">
-         <router-link class="navbar-brand" :to="{name: 'home'}"><a href="#"><img src="./../../../public/img/logo-white.png" class="img" alt=""/></a></router-link>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item" v-for="itemMenu in navMenu" :key="itemMenu.routeName">
+            <router-link class="ms_white" :to="{name: itemMenu.routeName}">{{itemMenu.label}}</router-link>
+            </li>
+        </ul>       
+    
+      <b-navbar-nav class="ml-auto">
+    
+        <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="ms_white" href="/admin">Accedi</a>
+              </li>
+            </ul>
 
-             <b-collapse id="nav-collapse" is-nav>
-
-               <ul class="navbar-nav mr-auto">
-
-                    <li class="nav-item" v-for="itemMenu in navMenu" :key="itemMenu.routeName">
-                        <router-link class="ms_white" :to="{name: itemMenu.routeName}">{{itemMenu.label}}</router-link>
-                    </li>
-
-                </ul>
-
-                
-                <b-navbar-nav class="ml-auto">
-                    <b-nav-form>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                        <a class="ms_white" href="/admin">Accedi</a>
-                        </li>
-                    </ul>
-                </b-nav-form>
-            </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-
-</header>
-
-   
-
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </header>
 </template>
 
   
@@ -58,19 +49,18 @@ export default {
  
  header{
    background-color: #ff8c28;
-   height: 80px;
+ 
  }
 
  img{
-    
+   width: 60px;
  }
 
  .ms_white{
    color: #fff;
    font-weight: bold;
-   font-size: 20px;
+   font-size: 15px;
  }
+
 </style>
 
-
- 
